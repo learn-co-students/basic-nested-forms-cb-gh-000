3fc973911bfae4d1c2b5e65db3cacc2d2c6496b6
Adrián Daraš <getpingui@gmail.com>
2017-03-18 18:27:11 +0100

    Done.

---
+++ spec/math_spec.rb
@@ -0,0 +1,11 @@
+describe "simple math" do
+  describe "2 + 2" do
+    it "isn't 5" do
+      expect(2 + 2).to_not be(5)
+    end
+
+    it "is 4" do
+      expect(2 + 2).to be(4)
+    end
+  end
+end
